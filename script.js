b4ca8fbc269af1f6bea99a4213f3413f34a4a5c8
thereisatablehere <dylan.bartness@gmail.com>
2024-03-19 07:18:38 -0500

    circles + colors

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-// 18 x 9
 let c;
 let r;
 let w, h;
-const CIRCLE_SIZE = 25;
-const LAUNCH_SPEED = 15;
-let currentColor = "orchid";
+
+const CIRCLES_MAX_COUNT_HORIZONTAL = 18;
+const CIRCLES_STARTING_ROWS_COUNT = 9;
+const CIRCLES_SPACING = 3;
+let circleSize = -1;
+
+window.onload = () => {
+    c = document.querySelector("canvas");
+    r = c.getContext("2d");
+    w = parseInt(window.getComputedStyle(c, null).getPropertyValue("width"));
+    h = parseInt(window.getComputedStyle(c, null).getPropertyValue("width"));
+    c.width = w;
+    c.height = h;
+
+    // circleSize = (CIRCLES_MAX_COUNT_HORIZONTAL + CIRCLES_SPACING);
+    circleSize = w / ((CIRCLES_MAX_COUNT_HORIZONTAL * 2) + CIRCLES_SPACING);
+    aimer.smallerSize = circleSize / 3;
+    
+    c.addEventListener("mousemove", setMouseAngle);
+    c.addEventListener("click", launch);
+    window.setInterval(loop, 1000 / 30);
+}
+
 /**
  * mouse angle only gets updated when mouse is moved, 
  * so if click to launch without ever moving mouse the 
@@ -16,6 +35,8 @@ let movedMouse = false;
 // 90 degrees, start by aiming straight up (for aimer only)
 let mouseAngle = 1.5708;
 let mouseXpos = false;
+const LAUNCH_SPEED = 15;
+
 let launched = {
     x: -1, 
     y: -1, 
@@ -27,24 +48,25 @@ let launched = {
         this.y += this.moveY;
 
         // bounce off wall
-        if((this.x > w - CIRCLE_SIZE) || (this.x < CIRCLE_SIZE)) {
+        if((this.x > w - circleSize) || (this.x < circleSize)) {
             this.moveX *= -1;
         }
 
         drawCircle(this.x, this.y, 
-            currentColor, CIRCLE_SIZE);
+            currentColor, circleSize);
     }
 };
+
 let aimer = {
-    smallerSize: CIRCLE_SIZE / 3, 
+    smallerSize: -1, 
     spacing: 23, 
     count: 3, 
     draw: function() {
-        let x = (w / 2) - (CIRCLE_SIZE / 2);
-        let y = h - (CIRCLE_SIZE / 2);
+        let x = (w / 2) - (circleSize / 2);
+        let y = h - (circleSize / 2);
 
-        let firstMoveX = Math.cos(mouseAngle) * (CIRCLE_SIZE / 1.75);
-        let firstMoveY = Math.sin(mouseAngle) * (CIRCLE_SIZE / 1.75);
+        let firstMoveX = Math.cos(mouseAngle) * (circleSize / 1.75);
+        let firstMoveY = Math.sin(mouseAngle) * (circleSize / 1.75);
 
         let moveX = Math.cos(mouseAngle) * this.spacing;
         let moveY = Math.sin(mouseAngle) * this.spacing;
@@ -58,7 +80,7 @@ let aimer = {
 
         // main circle
         drawCircle(x, y, 
-            currentColor, CIRCLE_SIZE);
+            currentColor, circleSize);
 
         x += firstMoveX;
         y += firstMoveY;
@@ -77,17 +99,62 @@ let aimer = {
     }   
 }
 
-window.onload = () => {
-    c = document.querySelector("canvas");
-    r = c.getContext("2d");
-    w = parseInt(window.getComputedStyle(c, null).getPropertyValue("width"));
-    h = parseInt(window.getComputedStyle(c, null).getPropertyValue("width"));
-    c.width = w;
-    c.height = h;
+let colors = {
+    current: null, 
+    bright: [
+        "#F06292", "#9575CD", "#4FC3F7", "#81C784", "#FF8A65"
+    ], 
+    pastel: [
+        "#F1948A", "#C39BD3", "#85C1E9", "#82E0AA", "#F5CBA7"
+    ]
+}
+colors.current = colors.bright;
+let currentColor = randomColor();
+
+function randomColor() {
+    let max = colors.current.length;
+    let min = 0;
+
+    return colors.current[Math.floor((Math.random() * (max - min)) + min)];
+}
+
+let circles = [];
+
+function initCircles() {
+    circles = [];
+
+    for(let row = 0; row < CIRCLES_STARTING_ROWS_COUNT; row++) {
+        let currentRow = [];
     
-    c.addEventListener("mousemove", setMouseAngle);
-    c.addEventListener("click", launch);
-    window.setInterval(loop, 1000 / 30);
+        for(let col = 0; col < CIRCLES_MAX_COUNT_HORIZONTAL; col++) {
+            currentRow.push(randomColor());
+        }
+    
+        circles.push(currentRow);
+    }
+}
+initCircles();
+
+function drawCircles() {
+    let x = -1;
+    let y = circleSize + CIRCLES_SPACING;
+
+    for(let row = 0; row < circles.length; row++) {
+        x = circleSize + CIRCLES_SPACING;
+
+        for(let col = 0; col < circles[0].length; col++) {
+            drawCircle(
+                x, 
+                y, 
+                circles[row][col], 
+                circleSize
+            );
+
+            x += (circleSize * 2) + CIRCLES_SPACING;
+        }
+        
+        y += (circleSize * 2) + CIRCLES_SPACING;
+    }
 }
 
 function setMouseAngle(e) {
@@ -111,8 +178,8 @@ function launch(e) {
         }
 
         launched.moving = true;
-        launched.x = (w / 2) - (CIRCLE_SIZE / 2);
-        launched.y = h - (CIRCLE_SIZE / 2);
+        launched.x = (w / 2) - (circleSize / 2);
+        launched.y = h - (circleSize / 2);
         
         launched.moveX = Math.cos(mouseAngle) * LAUNCH_SPEED;
         launched.moveY = Math.sin(mouseAngle) * LAUNCH_SPEED;
@@ -127,7 +194,7 @@ function launch(e) {
 function drawCircle(x, y, color, size) {
     r.fillStyle = color;
     r.beginPath();
-    r.arc(Math.round(x), Math.round(y), 
+    r.arc(x, y, 
         size, 
         0, 2 * Math.PI);
     r.fill();
@@ -136,6 +203,8 @@ function drawCircle(x, y, color, size) {
 
 function loop() {
     r.clearRect(0, 0, w, h);
+
+    drawCircles();
 
     if(launched.moving) {
         launched.update();
